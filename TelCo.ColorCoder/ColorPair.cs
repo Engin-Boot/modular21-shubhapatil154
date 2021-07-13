7b7004984a91affd5a1943f18bebdb97b2f4d8eb
Shubha Suresh Patil <shusures@publicisgroupe.net>
2021-07-13 17:18:37 +0530

    MainProgram  updated

--- TelCo.ColorCoder/ColorPair.cs
+++ TelCo.ColorCoder/ColorPair.cs
@@ -33,8 +33,7 @@ namespace TelCo.ColorCoder
             int minorIndex = zeroBasedPairNumber % minorSize;
 
             // Construct the return val from the arrays
-            ColorPair pair = new ColorPair() { majorColor = colorMapMajor[majorIndex],
-            	minorColor = colorMapMinor[minorIndex] };
+            ColorPair pair = new ColorPair() { majorColor = colorMapMajor[majorIndex],minorColor = colorMapMinor[minorIndex] };
             
             // return the value
             return pair;
